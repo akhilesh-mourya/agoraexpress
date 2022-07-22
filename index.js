6e70a7dbebc826a627d106632ba970a78a622a38
Akhilesh Mourya <mouryaakhilesh62@gmail.com>
2022-07-22 13:34:19 +0530

    Added post deploy build script

--- index.js
+++ index.js
@@ -25,19 +25,6 @@ app.use(express.static(__dirname + '/talamus'));
 app.get("/", (req, res) => res.send("Agora Auth Token Server"));
 
 
-app.post("/rtctoken", (req, res) => {
-    // Generate Token Here
-    const appID = "a12e74a9c9814071b316f55d43bad822";
-    const appCertificate = "88bb6308b3cc48e0bc614f43f7038470";
-    const uid = Math.floor(Math.random() * 100000);
-    const expirationTimeInSeconds = 3600;
-    const currentTimestamp = Math.floor(Date.now() / 1000);
-    const privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds;
-    const role = Agora.RtcRole.SUBSCRIBER;
-    const channel = req.body.channel;
-    const token = Agora.RtcTokenBuilder.buildTokenWithUid(appID, appCertificate, channel, uid, role, privilegeExpiredTs);
-    res.send({ uid, token });
-  });
 
   app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'))
---
+++ talamus/app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+
+router.post("/rtctoken", (req, res) => {
+    // Generate Token Here
+    const appID = "a12e74a9c9814071b316f55d43bad822";
+    const appCertificate = "88bb6308b3cc48e0bc614f43f7038470";
+    const uid = Math.floor(Math.random() * 100000);
+    const expirationTimeInSeconds = 3600;
+    const currentTimestamp = Math.floor(Date.now() / 1000);
+    const privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds;
+    const role = Agora.RtcRole.SUBSCRIBER;
+    const channel = req.body.channel;
+    const token = Agora.RtcTokenBuilder.buildTokenWithUid(appID, appCertificate, channel, uid, role, privilegeExpiredTs);
+    res.send({ uid, token });
+  });
+
+module.exports = router;
