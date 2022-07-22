fd8649a0416ac772b5cedcd4f89841edb182a0e5
Akhilesh Mourya <mouryaakhilesh62@gmail.com>
2022-07-22 16:35:30 +0530

    Added post deploy build script

--- index.js
+++ index.js
@@ -22,8 +22,9 @@ const db = getFirestore(firebaseApp);
 
 
 const app = express();
+app.set('port', (process.env.PORT || 3000))
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use(bodyParser.json());
+// app.use(bodyParser.json());
 app.use('/app', usersRouter);
 
 
@@ -48,7 +49,6 @@ app.use(function(err, req, res, next) {
 
 app.use(express.json());
 
-// app.set('port', (process.env.PORT || 3000))
 
 //   app.listen(app.get('port'), function() {
 //     console.log("Node app is running at localhost:" + app.get('port'))
